e2c8530ab8d903529372b5451a63f780d80ce3fd
Alison Mazzarella-Woelzl <104347449+AlisonMazzarella@users.noreply.github.com>
2023-03-28 11:09:59 +0200

    display names added and validation

--- Models/Meeting.cs
+++ Models/Meeting.cs
@@ -7,21 +7,42 @@ namespace SacramentPlanner.Models
     {
         public int Id { get; set; }
         [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
         public string? Congregation { get; set; }
         [DataType(DataType.Date)]
+
+        [Display(Name = "Meeting Date")]
         public DateTime MeetingDate { get; set; }
         [Required]
+        [StringLength(60, MinimumLength = 3)]
         public string? Conducting { get; set; }
         [Required]
+
+        [StringLength(60, MinimumLength = 3)]
+        [Display(Name = "Opening Prayer")]
         public string? OpeningPrayer { get; set; }
         [Required]
+
+        [StringLength(60, MinimumLength = 3)]
+        [Display(Name = "Closing Prayer")]
         public string? ClosingPrayer { get; set; }
         [Required]
+
+        [Display(Name = "Opening Hymn")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
         public string? OpeningHymn { get; set; }
         [Required]
+
+        [Display(Name = "Sacrament Hymn")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
         public string? SacramentHymn { get; set; }
+        [Display(Name = "Intermediate Hymn")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
         public string? IntermediateHymn { get; set; }
         [Required]
+
+        [Display(Name = "Closing Hymn")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
         public string? ClosingHymn { get; set; }
     }
 }
