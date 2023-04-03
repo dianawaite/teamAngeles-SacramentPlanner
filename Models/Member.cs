92604e6ac25cefad25a6ead75d27b1d1557159d7
blackadder-git <blackadder-git@email.com>
2023-04-03 14:40:31 -0700

    added table hover - JC

--- Models/Member.cs
+++ Models/Member.cs
@@ -7,7 +7,6 @@ namespace SacramentPlanner.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        [Display(Name = "Member")]
         //[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public string? Name { get; set; }
 
